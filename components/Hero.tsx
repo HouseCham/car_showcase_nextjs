3923ed6f8ee7e6efa7357cc76cddca0c12e872dd
HouseCham <ramsesramirezvallejo@gmail.com>
2023-07-07 01:20:41 -0600

    Hero component started to get structure

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,16 @@ import Image from "next/image"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Unlock Your Journey, Book and Drive with Us!
+        </h1>
+        <p className="hero__subtitle">
+          We provide hassle-free car rentals and sales, allowing you to embark on unforgettable journeys with ease.
+        </p>
+      </div>
+    </div>
   )
 }
 
