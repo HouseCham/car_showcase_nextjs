9f9521c480bc4285d6b63985e72bbd238116c098
HouseCham <ramsesramirezvallejo@gmail.com>
2023-07-09 13:55:49 -0600

    customButton component created

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,36 @@
+'use client'
+
 import Image from "next/image"
+import CustomButton from "./CustomButton"
 
 // rafce
 
 const Hero = () => {
+  const handleScroll = () => {}
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
           Unlock Your Journey, Book and Drive with Us!
         </h1>
+
         <p className="hero__subtitle">
           We provide hassle-free car rentals and sales, allowing you to embark on unforgettable journeys with ease.
         </p>
+
+        <CustomButton
+          title="See more"
+          containerStyle="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"/>
+        </div>
       </div>
     </div>
   )
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+import { MouseEventHandler } from "react";
+
+// Custom Button Props Interface corresponds to the props of the Custom Button Component
+export interface CustomButtonProps {
+    title: string;
+    containerStyle?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
